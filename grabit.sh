fbcdfc37bc1ecda28301c3bf09008e9d40b106b6
Marcel Fortin <marcel.fortin@utoronto.ca>
2015-09-28 14:40:31 -0400

    added a shell script that works more or less

---
+++ grabit.sh
@@ -0,0 +1,3 @@
+wget -o "animal_processing_1892_$(date +%Y%m%d).zip" http://maps.library.utoronto.ca/dvhmp/data/2009_05_10/industry/animalprocessing_1892.zip
+
+#this seems to be acting funny. it downloads the file completely and then creates a new file with the new assigned name, but the file is not the same size.
